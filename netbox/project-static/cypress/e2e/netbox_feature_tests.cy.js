64f09ef8eb234307c3e08d4a7289b430f10fdb88
cgiestas <christianegiestas@gmail.com>
2026-07-16 21:54:24 -0300

    ci(testes): modifica a forma de fazer login nos testes

--- netbox/project-static/cypress/e2e/netbox_feature_tests.cy.js
+++ netbox/project-static/cypress/e2e/netbox_feature_tests.cy.js
@@ -1,45 +1,53 @@
+Cypress.Commands.add('loginAsAdmin', () => {
+  cy.session('admin-session', () => {
+    cy.visit('http://localhost:8000')
+    const username = Cypress.env('NETBOX_USERNAME') || 'admin'
+    const password = Cypress.env('NETBOX_PASSWORD') || 'admin'
+    cy.get('#id_username', { timeout: 10000 }).type(username)
+    cy.get('#id_password').type(`${password}{enter}`)
+    cy.url().should('eq', 'http://localhost:8000/')
+  })
+})
+
+Cypress.Commands.add('hideDebugToolbar', () => {
+  cy.get('body').then($body => {
+    if ($body.find('#djDebug').length) {
+      cy.get('#djDebug').invoke('css', 'display', 'none')
+    }
+  })
+})
+
 describe('NetBox - Device Type Image Columns', () => {
 
   beforeEach(() => {
-    //Visit your local NetBox instance 
-    cy.visit('http://localhost:8000', { timeout: 15000 })
-
-    const username = Cypress.env('NETBOX_USERNAME') || 'admin'
-    const password = Cypress.env('NETBOX_PASSWORD') || 'admin'
-    //Log in
-    cy.get('#id_username', { timeout: 10000 }).type(username)
-    cy.get('#id_password').type(`${password}{enter}`)
-    //esconde a debug toolbar, se existir
-    cy.get('body').then($body => {
-      if ($body.find('#djDebug').length) {
-        cy.get('#djDebug').invoke('css', 'display', 'none')
-      }
-    })
+    cy.loginAsAdmin()
   })
 
-it('should allow a user to add Front Image, Rear Image, and Image Counter columns', () => {
-  cy.visit('http://localhost:8000/dcim/device-types/')
-  cy.contains('button', 'Configure Table').click()
+  it('should allow a user to add Front Image, Rear Image, and Image Counter columns', () => {
+    cy.visit('http://localhost:8000/dcim/device-types/')
+    cy.hideDebugToolbar()
 
-  // Seleciona cada coluna na lista "Available Columns" e clica em "Adicionar"
-  cy.get('#id_columns').select('front_image')
-  cy.contains('button', 'Adicionar').click()
+    cy.contains('button', 'Configure Table').click()
 
-  cy.get('#id_columns').select('rear_image')
-  cy.contains('button', 'Adicionar').click()
+    cy.get('#id_columns').select('front_image')
+    cy.contains('button', 'Adicionar').click()
 
-  cy.get('#id_columns').select('image_count')
-  cy.contains('button', 'Adicionar').click()
+    cy.get('#id_columns').select('rear_image')
+    cy.contains('button', 'Adicionar').click()
 
-  cy.contains('button', 'Aplicar').click()
+    cy.get('#id_columns').select('image_count')
+    cy.contains('button', 'Adicionar').click()
 
-  cy.get('table th').should('contain', 'Front Image')
-  cy.get('table th').should('contain', 'Rear Image')
-  cy.get('table th').should('contain', 'Imagens')
-})
+    cy.contains('button', 'Aplicar').click()
+
+    cy.get('table th').should('contain', 'Front Image')
+    cy.get('table th').should('contain', 'Rear Image')
+    cy.get('table th').should('contain', 'Imagens')
+  })
 
   it('should allow a user to create a new Site successfully', () => {
     cy.visit('http://localhost:8000/dcim/sites/add/')
+    cy.hideDebugToolbar()
     cy.get('#id_name').type('Segundo Data Center ICEA')
     cy.get('button[name="_create"]').click()
     cy.url().should('include', '/dcim/sites/')
@@ -48,6 +56,7 @@ it('should allow a user to add Front Image, Rear Image, and Image Counter column
 
   it('should allow a user to create a new VLAN successfully', () => {
     cy.visit('http://localhost:8000/ipam/vlans/add/')
+    cy.hideDebugToolbar()
     cy.get('#id_vid').type('200')
     cy.get('#id_name').type('VLAN-TESTE-NOVO')
     cy.get('button[name="_create"]').click()
